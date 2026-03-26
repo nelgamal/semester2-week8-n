9bfea18b6d66cbb0231dbf2e90404ed964e05a91
nelgamal <znhg0906@leeds.ac.uk>
2026-03-26 18:55:35 +0000

    end of session 1

--- session-1/task3/shapes.c
+++ session-1/task3/shapes.c
@@ -6,28 +6,43 @@ int main( void ) {
 
     // complete the structure definition in the header file first
     // code in main to test the structures and functions
+    Point p = {.x=1, .y=2}; 
     
+    Rectangle r = makeRectangle(p, 4.0, 3.0);
+
+    printf("Area: %.2f\n", area(r));
+
+    Point dp = {.x=2, .y=1};
+    shiftRectangle(&r, dp);
+
+    printf("Shifted position: (%.1f, %.1f)\n", r.p.x, r.p.y);
+
+    scaleRectangle(&r, 2.0);
+
+    printf("Scaled width: %.1f, height: %.1f\n", r.width, r.height);
     return 0;
 }
 
 Rectangle makeRectangle( Point p, float width, float height ){
     Rectangle r;
 
+    r.p = p;
+    r.width = width; 
+    r.height = height; 
+
     return r;
 }
 
 float area( Rectangle r ) {
-    float a = 0.0;
-
-    return a;
+    return r.width * r.height;
 }
 
 void shiftRectangle( Rectangle *r, Point dp ) {
-
-    return;
+    r ->p.x += dp.x;
+    r ->p.y += dp.y;
 }
 
 void scaleRectangle( Rectangle *r, float scale ) {
-
-    return;
+    r ->width *= scale; 
+    r -> height *= scale; 
 }
--- session-1/task3/shapes.h
+++ session-1/task3/shapes.h
@@ -9,7 +9,9 @@ typedef struct _point {
 // define the Rectangle structure here
 
 typedef struct _rectangle {
-    // fields
+    Point p; 
+    float width;
+    float height;
 } Rectangle;
 
 // function headers - implement in shapes.c
