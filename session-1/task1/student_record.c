2bddb235815b789d837f2aa29dc94324b253bc61
nelgamal <znhg0906@leeds.ac.uk>
2026-03-26 03:22:33 +0000

    End of task1

--- session-1/task1/student_record.c
+++ session-1/task1/student_record.c
@@ -20,7 +20,9 @@ float calculate_average(struct student_record student) {
     // TODO: Implement this function to calculate and return the average
     // of the three module marks
     
-    return 0.0; // Placeholder return value
+    float sum = student.marks[0] + student.marks[1] + student.marks[2];
+
+    return sum / 3.0; 
 }
 
 /**
@@ -31,6 +33,18 @@ float calculate_average(struct student_record student) {
 void display_student(struct student_record student) {
     // TODO: Implement this function to display student information
     // Format: Name, ID, individual marks, and average mark
+
+    printf("Name: %s\n", student.name);
+    printf("ID: %d\n", student.id);
+
+    printf("Marks: %.1f %.1f %.1f\n",
+    student.marks[0],
+    student.marks[1],
+    student.marks[2]);
+
+    float avg = calculate_average(student);
+    printf("Average: %.2f\n", avg);
+
 }
 
 /*
@@ -43,6 +57,14 @@ int main( void ) {
     struct student_record student;
     
     // TODO: display the student data and average mark
-    
+    strcpy(student.name, "John Smith");
+    student.id = 12345;
+
+    student.marks[0] = 75.0; 
+    student.marks[1] = 68.5; 
+    student.marks[2] = 81.0;
+
+    display_student(student); 
+
     return 0;
 }
